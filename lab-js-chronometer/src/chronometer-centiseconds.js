65485d873c3af9fab1908957ef8d499561290751
Marco Lölkes <marcoloelkes@gmx.de>
2023-01-18 14:49:09 +0100

    Add solution for lab-js-chronometer

---
+++ lab-js-chronometer/src/chronometer-centiseconds.js
@@ -0,0 +1,46 @@
+class Chronometer {
+    constructor() {
+        this.currentTime = 0
+        this.intervalId = null
+    }
+
+    start(printTimeCallback) {
+        this.intervalId = setInterval(() => {
+            this.currentTime++
+            if (printTimeCallback) printTimeCallback()
+        }, 10)
+    }
+
+    getMinutes() {
+        return Math.floor(this.currentTime / 6000)
+    }
+
+    getSeconds() {
+        return Math.floor(this.currentTime / 100) % 60
+    }
+
+    getCentiseconds() {
+        return this.currentTime % 100
+    }
+
+    computeTwoDigitNumber(value) {
+        if (value < 10) return `0${value}`
+        return `${value}`
+    }
+
+    stop() {
+        clearInterval(this.intervalId)
+    }
+
+    reset() {
+        this.currentTime = 0
+    }
+
+    split() {
+        const minutes = this.computeTwoDigitNumber(this.getMinutes())
+        const seconds = this.computeTwoDigitNumber(this.getSeconds())
+        const centiseconds = this.computeTwoDigitNumber(this.getCentiseconds())
+        
+        return `${minutes}:${seconds}:${centiseconds}`
+    }
+}
---
+++ lab-js-chronometer/src/chronometer.js
@@ -0,0 +1,41 @@
+class Chronometer {
+    constructor() {
+        this.currentTime = 0
+        this.intervalId = null
+    }
+
+    start(printTimeCallback) {
+        this.intervalId = setInterval(() => {
+            this.currentTime++
+            if (printTimeCallback) printTimeCallback()
+        }, 1000)
+    }
+
+    getMinutes() {
+        return Math.floor(this.currentTime / 60)
+    }
+
+    getSeconds() {
+        return this.currentTime % 60
+    }
+
+    computeTwoDigitNumber(value) {
+        if (value < 10) return `0${value}`
+        return `${value}`;
+    }
+
+    stop() {
+        clearInterval(this.intervalId)
+    }
+
+    reset() {
+        this.currentTime = 0
+    }
+
+    split() {
+        const minutes = this.computeTwoDigitNumber(this.getMinutes())
+        const seconds = this.computeTwoDigitNumber(this.getSeconds())
+    
+        return `${minutes}:${seconds}`
+    }
+}
